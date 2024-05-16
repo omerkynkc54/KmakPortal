26127f19fe9c5ab559b48095b1b3d57c82fff23c
omerkynkc54 <omerkay54@gmail.com>
2024-05-16 14:28:53 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KmakPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
